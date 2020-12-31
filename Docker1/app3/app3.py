06d9f321b96ee652b6fadc5e3d5f9470b2a5907a
[aziz604191] <[aziz604191@gmail.com]>
2020-12-31 00:31:20 -0500

    Docker Devops Code challenge

---
+++ Docker1/app3/app3.py
@@ -0,0 +1,14 @@
+from flask import request, Flask
+import json
+
+
+app3 = Flask(__name__)
+
+
+@app3.route('/')
+def hello_world():
+    return 'Welcome, this is Application3 :) '
+
+
+if __name__ == '__main__':
+   app3.run(debug=True, host='0.0.0.0')
